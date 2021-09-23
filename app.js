59285419a9ee4f7455a893e192d5f554a0624cf7
sos2020-hash <qweiop525@gmail.com>
2021-09-23 19:46:18 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+const people = require("./routes/people");
+const login = require("./routes/login");
+
+app.use(express.static("./methods-public"));
+
+app.use(express.urlencoded({ extended: false }));
+
+app.use("/api/people", people);
+app.use("/login", login);
+
+app.listen(5000, () => {
+  console.log("server is listening on port 5000...");
+});
